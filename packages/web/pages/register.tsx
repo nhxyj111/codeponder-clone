ff148d64b946c56b9ab8c4e49d9db6910b39dd75
xuyejian <nhxyj111@gmail.com>
2019-02-17 14:24:44 +0800

    next + apollo

---
+++ packages/web/pages/register.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Layout from "../components/Layout";
+
+const AboutPage: React.FunctionComponent = () => (
+  <Layout title="Register">
+    <h1>Register</h1>
+  </Layout>
+);
+
+export default AboutPage;
