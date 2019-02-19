ad73c7d51383f2c950b0b7386cb06beec8db1409
xuyejian <nhxyj111@gmail.com>
2019-02-19 17:01:05 +0800

    landing page

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,14 +1,38 @@
-import * as React from 'react'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from "next/link";
+import * as React from "react";
+import { Button } from "semantic-ui-react";
+import Layout from "../components/Layout";
 
 const IndexPage: React.FunctionComponent = () => {
   return (
-    <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Hello Next.js 👋</h1>
-      <p><Link href='/about'><a>About</a></Link></p>
+    <Layout title="Landing Page | Code Ponder">
+      <h1>Code Ponder 👋</h1>
+      <h4>Marketplace For Code Reviews</h4>
+      <h2>How it works</h2>
+      <ol>
+        <li>Coder requests a code review</li>
+        <li>Expert offer assistance</li>
+        <li>Coder picks favorite expert</li>
+        <li>Selected expert reviews request to review the code on Github</li>
+      </ol>
+
+      <p>
+        <Link href="/register">
+          <a>
+            <Button>Register as a Coder</Button>
+          </a>
+        </Link>
+      </p>
+
+      <p>
+        <Link href="/register">
+          <a>
+            <Button>Register as a Expert</Button>
+          </a>
+        </Link>
+      </p>
     </Layout>
-  )
-}
+  );
+};
 
 export default IndexPage;
